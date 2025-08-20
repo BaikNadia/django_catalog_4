581a119a127266249fc0fe6f4feb95b0a20b3c33
BaikNadia <baikowa.nadia@gmail.com>
2025-08-20 14:49:52 +0300

    change urls

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,21 +1,26 @@
-# from django.urls import path
-# from .apps import CatalogConfig
-# from .views import ProductListView, ProductDetailView
-#
-# app_name = CatalogConfig.name
-#
-# urlpatterns = [
-#     path('', ProductListView.as_view(), name='product_list'),
-#     path('products/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
-# ]
-
 from django.urls import path
 from . import views
 
 app_name = 'catalog'
 
 urlpatterns = [
-    path('', views.HomeView.as_view(), name='home'),           # /catalog/ → главная с баннером
-    path('products/', views.ProductListView.as_view(), name='product_list'),  # /catalog/products/ → список товаров
-    path('products/<int:pk>/', views.ProductDetailView.as_view(), name='product_detail'),
+    path('', views.HomeView.as_view(), name='home'),
+    path('products/', views.ProductListView.as_view(), name='product_list'),
+    path('product/<int:pk>/', views.ProductDetailView.as_view(), name='product_detail'),
+    path('product/create/', views.ProductCreateView.as_view(), name='product_create'),
+    path('product/<int:pk>/update/', views.ProductUpdateView.as_view(), name='product_update'),
+    path('product/<int:pk>/delete/', views.ProductDeleteView.as_view(), name='product_delete'),
 ]
+
+
+# from django.urls import path
+# from . import views
+#
+# app_name = 'catalog'
+#
+# urlpatterns = [
+#     path('', views.HomeView.as_view(), name='home'),           # /catalog/ → главная с баннером
+#     path('products/', views.ProductListView.as_view(), name='product_list'),  # /catalog/products/ → список товаров
+#     path('products/<int:pk>/', views.ProductDetailView.as_view(), name='product_detail'),
+# ]
+
