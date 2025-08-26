041f55a14851ac8dd93f215b0d80d5faa7e60a78
BaikNadia <baikowa.nadia@gmail.com>
2025-08-26 21:58:19 +0300

    change catalog/view

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,7 @@
-from django.contrib import messages
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.views.generic import ListView, DetailView, CreateView
 from django.views.generic import UpdateView, DeleteView, TemplateView
+from django.core.exceptions import PermissionDenied
 from django.urls import reverse_lazy
 from .models import Product
 from .forms import ProductForm
@@ -15,10 +15,27 @@ class ProductListView(ListView):
     template_name = 'catalog/product_list.html'
     context_object_name = 'products'
 
+    def get_queryset(self):
+        # ✅ Правильно: все опубликованные товары
+        return Product.objects.filter(publication_status='published')
+
 class ProductDetailView(DetailView):
     model = Product
     template_name = 'catalog/product_detail.html'
     context_object_name = 'product'
+
+class ProductUnpublishView(PermissionRequiredMixin, UpdateView):
+    model = Product
+    fields = []  # не редактируем, только действие
+    template_name = 'catalog/product_confirm_unpublish.html'
+    success_url = reverse_lazy('catalog:product_list')
+    permission_required = 'catalog.can_unpublish_product'
+
+    def form_valid(self, form):
+        product = form.instance
+        product.is_published = False
+        product.save()
+        return super().form_valid(form)
 
 class ProductCreateView(LoginRequiredMixin, CreateView):
     model = Product
@@ -27,7 +44,8 @@ class ProductCreateView(LoginRequiredMixin, CreateView):
     success_url = reverse_lazy('catalog:product_list')
 
     def form_valid(self, form):
-        messages.success(self.request, f"Товар '{form.cleaned_data['name']}' успешно создан.")
+
+        form.instance.owner = self.request.user
         return super().form_valid(form)
 
 
@@ -36,24 +54,24 @@ class ProductUpdateView(LoginRequiredMixin, UpdateView):
     model = Product
     form_class = ProductForm
     template_name = 'catalog/product_form.html'
+    success_url = reverse_lazy('catalog:product_list')
 
-    def get_success_url(self):
-        return reverse_lazy('catalog:product_detail', kwargs={'pk': self.object.pk})
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+        if obj.owner != self.request.user:
+            raise PermissionDenied("Вы не можете редактировать этот товар.")
+        return obj
 
-    def form_valid(self, form):
-        response = super().form_valid(form)
-        messages.success(
-            self.request,
-            f"Товар '{form.cleaned_data['name']}' успешно обновлён."
-        )
-        return response
+
 
 class ProductDeleteView(LoginRequiredMixin, DeleteView):
     model = Product
     template_name = 'catalog/product_confirm_delete.html'
     success_url = reverse_lazy('catalog:product_list')
 
-    def form_valid(self, form):
-        product = self.get_object()
-        messages.success(self.request, f"Товар '{product.name}' успешно удалён.")
-        return super().form_valid(form)
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+        # Удалять может: владелец ИЛИ модератор
+        if obj.owner != self.request.user and not self.request.user.has_perm('catalog.delete_product'):
+            raise PermissionDenied("У вас нет прав на удаление этого товара.")
+        return obj
